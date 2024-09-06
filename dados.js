e39d043630d6a54896f345580a74116660e4aef4
Igor Paulino <igr.paulino@gmail.com>
2024-09-06 12:32:43 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,33 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal: A Fadinha do Skate. Jhulia Rayssa Mendes Leal, conhecida como Rayssa Leal, é uma skatista brasileira que ganhou destaque internacionalmente ao conquistar a medalha de prata nos Jogos Olímpicos de Tóquio 2020 e a medalha de bronze nos Jogos Olímpicos de Paris 2024. Com apenas 16 anos, Rayssa já é campeã mundial e pan-americana, sendo uma das maiores promessas do esporte mundial. Sua trajetória é marcada por talento, dedicação e uma paixão contagiante pelo esporte.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8 skate feminino olimpíadas tokyo 2020 esporte radical jovem talento brasileira"
+
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza: A nova promessa do judô brasileiro. Beatriz Souza é uma judoca brasileira que se destacou nas competições nacionais e internacionais, conquistando diversas medalhas. Sua técnica apurada e determinação a colocam como uma das principais atletas da nova geração do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judoca brasileira campeã mundial pan-americana artes marciais"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade: A rainha da ginástica artística. Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros ao conquistar a medalha de prata no individual geral nos Jogos Olímpicos de Tóquio 2020. Sua versatilidade e força a tornam uma das principais atletas da ginástica mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica artística ginasta brasileira olimpíadas tokyo 2020 individual geral força versatilidade"
+    },
+    {
+        titulo: "Martine Grael",
+        descricao: "Martine Grael: A rainha das velas. Martine Grael é uma velejadora brasileira que, ao lado de Kahena Kunze, conquistou a medalha de ouro na classe 49er FX nos Jogos Olímpicos de Tóquio 2020. Sua habilidade e experiência a consolidaram como uma das maiores velejadoras do mundo.",
+        link: "https://exemplo.com/martine-grael",
+        tags: "vela velejadora brasileira olimpíadas tokyo 2020 49er FX esporte aquático"
+      },
+      {
+        titulo: "Alison dos Santos",
+        descricao: "Alison dos Santos: O Piu, fenômeno do atletismo. Alison dos Santos, conhecido como Piu, é um atleta brasileiro especializado em 400 metros com barreiras. Ele conquistou a medalha de bronze nos Jogos Olímpicos de Tóquio 2020 e é considerado uma das maiores promessas do atletismo mundial.",
+        link: "https://exemplo.com/alison-dos-santos",
+        tags: "atletismo 400 metros com barreiras piu brasileiro olimpíadas tokyo 2020"
+      }
+];
